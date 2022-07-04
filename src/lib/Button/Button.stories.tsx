aa790f5ffe7fca9ada67a7e3a81c7ac00dde5c33
HerilS <herilsaha06@gmail.com>
2022-07-03 19:41:30 -0600

    feat(storybook setup): initialized storybook

---
+++ src/lib/Button/Button.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Button } from './Button';
+
+export default {
+  title: 'Button',
+  component: Button,
+};
+
+export const Primary = () => <Button>hello</Button>;
